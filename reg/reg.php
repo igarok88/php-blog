dff9e63084e66dff9b418e18335734bb2a896b60
Ihor <usd.wmz@gmail.com>
2025-06-21 13:24:21 +0200

    adding users to db using ajax library

--- reg/reg.php
+++ reg/reg.php
@@ -5,13 +5,18 @@ $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
 $login = trim(filter_var($_POST['login'], FILTER_SANITIZE_STRING));
 $pass = trim(filter_var($_POST['pass'], FILTER_SANITIZE_STRING));
 
+$error = '';
 if (strlen($username) <= 3) {
-    exit();
+    $error = "Введите имя";
 } elseif (strlen($email) <= 3) {
-    exit();
+    $error = "Введите Email";
 } elseif (strlen($login) <= 3) {
-    exit();
+    $error = "Введите логин";
 } elseif (strlen($pass) <= 3) {
+    $error = "Введите палорь";
+}
+if ($error != '') {
+    echo $error;
     exit();
 }
 
@@ -28,3 +33,5 @@ $pdo = new PDO($dsn, $user, $password);
 $sql = 'INSERT INTO users(name, email, login, pass) VALUES(?, ?, ?, ?)';
 $query = $pdo->prepare($sql);
 $query->execute([$username, $email, $login, $pass]);
+
+echo 'Готово';
